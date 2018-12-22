ae3a76eed0c81d609b0cd3adb124d4652123e048
Augustin Godiscal <dev@auggod.io>
2018-12-23 00:53:09 +0100

    Set latitude and longitude instead

--- query/index.js
+++ query/index.js
@@ -1,9 +1,16 @@
-const { Coordinates } = require('../location')
-
 const Query = {
   type: 'object',
   properties: {
-    coordinates: Coordinates,
+    latitude: {
+      type: 'number',
+      maximum: 90,
+      minimum: -90
+    },
+    longitude: {
+      type: 'number',
+      maximum: 180,
+      minimum: -180
+    },
     limit: {
       type: 'number',
       maximum: 100
