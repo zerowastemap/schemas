0a2317306a6e2ca1f9c466f06e56953c7adef078
Augustin Godiscal <dev@auggod.io>
2018-12-23 00:53:23 +0100

    Add user

---
+++ user/index.js
@@ -0,0 +1,33 @@
+const Email = {
+  type: 'string',
+  minLength: 5,
+  maxLength: 256
+}
+
+const Username = {
+  type: 'string',
+  minLength: 1,
+  maxLength: 48,
+  pattern: '^[a-z][a-z0-9_-]*$'
+}
+
+const Role = {
+  type: 'string',
+  enum: ['user', 'admin']
+}
+
+const User = {
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    username: Username,
+    role: Role,
+    email: Email
+  }
+}
+
+module.exports = {
+  Email,
+  Role,
+  User
+}
